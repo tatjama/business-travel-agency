de9102bcd030535003327796cf9d02b044daa9a5
tatjama <tanja120a@gmail.com>
2020-08-07 15:18:51 +0200

    refactoring sign up function
    class is remove from signUp function infront of it, status identificator add

--- js/index.js
+++ js/index.js
@@ -10,7 +10,6 @@ function showSignInForm() {
     goSignUpForm.style.display = "block";
     goSignInForm.style.display = "none";
 }
-
 //pop-up SIGN UP
 function showSignUpForm() {
     signInForm.style.display = "none";
@@ -18,13 +17,10 @@ function showSignUpForm() {
     goSignUpForm.style.display = "none";
     goSignInForm.style.display = "block";
 }
-
 function hideSignUpForm() {
     signUpForm.style.display = "none";
 }
-
 //Validation 
-
 function validEMail(a, b, c) {
     let val = document.getElementById(a).value;
     if (!(/[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+/.test(val)) || (val == '')) {
@@ -49,12 +45,11 @@ function validPhone(a, b, c) {
         document.getElementsByClassName(c)[b].innerHTML = '*';
     }
 }
-//brise * kada je onfocus polje u koje treba da unesemo ispravku
+//remove  * when input field is on focus
 function clearOnFocus(a, b) {
     document.getElementsByClassName(a)[b].innerHTML = '';
 }
 function clearSignUp() {
-
     document.getElementById('sign-up-name').value = '';
     document.getElementById('sign-up-surname').value = '';
     document.getElementById('sign-up-e-mail').value = '';
@@ -64,100 +59,79 @@ function clearSignUp() {
     document.getElementById('phone1').value = '';
     document.getElementById('phone2').value = '';
     document.getElementById('sign-up-password-r').value = '';
-    document.getElementById('kolicinaRegistracija').value = '1';
-
-
+    document.getElementById('status').value = '1';
 }
+
 //SIGN UP         
 //funkcija prikuplja podatke iz forme u HTML-u i smesta u objekat sa nazivom noviKorisnik.
 //potom ubacuje metodom push objekat noviKorisnik u niz nizKorisnika gde ga pamti
 //Istovremeno sakriva dugme Pokupi  da ne bismo mogli greskom da prepisemo jednog korisnika drugim 
 // Inicijalizujemo promenljivu nizKorisnika kao niz u koji smestamo dobijene objekte
-var arrayUsers = [];
+      
+class User{
+    constructor(name, surname, email, password, address1, address2, phone1, phone2){
+        this.name = name,
+        this.surname = surname,
+        this.email = email,
+        this.password = password,
+        this.address1 = address1,
+        this.address2 = address2,
+        this.phone1 = phone1,
+        this.phone2 = phone2,
+        this.status = 1
+    }
+
+    static checkDoesUserExist(user, arrayUsers){
+        arrayUsers.forEach(element => {
+            if (element.email === user.email) {
+                alert('We alredy have that user in DB.')
+                document.getElementById('sign-up-e-mail').value = '';
+                user.email = '';
+            }        
+        })
+    }
+
+}
 
 function signUp() {
-    var arrayUsers = JSON.parse(localStorage.getItem('localStorageUsers')) || [];
-    console.log(arrayUsers);
+    var arrayUsers = JSON.parse(localStorage.getItem('localStorageUsers')) || [];    
+    let name = document.getElementById('sign-up-name').value.toUpperCase();
+    let surname = document.getElementById('sign-up-surname').value.toUpperCase();
+    let email = document.getElementById('sign-up-e-mail').value;
+    let password = document.getElementById('sign-up-password').value;
+    let passwordR = document.getElementById('sign-up-password-r').value;
+    let address1 = document.getElementById('address1').value;
+    let address2 = document.getElementById('address2').value;
+    let phone1 = document.getElementById('phone1').value;
+    let phone2 = document.getElementById('phone2').value;   
 
-    if (document.getElementById('sign-up-name').value == '' ||
-        document.getElementById('sign-up-surname').value == '' ||
-        document.getElementById('sign-up-e-mail').value == '' ||
-        document.getElementById('sign-up-password').value == '' ||
-        document.getElementById('sign-up-password-r').value == '' ||
-        document.getElementById('address1').value == '' ||
-        document.getElementById('phone1').value == '' ||
+    if (name == '' || surname == '' || email == '' || password == '' ||
+        passwordR == '' || address1 == '' || phone1 == '' || 
         document.getElementsByClassName('error-sign-up')[0].innerHTML != '' ||
         document.getElementsByClassName('error-sign-up')[1].innerHTML != '' ||
         document.getElementsByClassName('error-sign-up')[2].innerHTML != '' ||
         document.getElementsByClassName('error-sign-up')[3].innerHTML != '' ||
         document.getElementsByClassName('error-sign-up')[4].innerHTML != '' ||
         document.getElementsByClassName('error-sign-up')[5].innerHTML != '' ||
-        document.getElementsByClassName('error-sign-up')[7].innerHTML != '') {
-        //  alert('Neispravan unos ili prazno polje');
+        document.getElementsByClassName('error-sign-up')[7].innerHTML != '') {        
         alert('invalid input')
-    } else {
-        let name = document.getElementById('sign-up-name').value.toUpperCase();
-        let surname = document.getElementById('sign-up-surname').value.toUpperCase();
-        let email = document.getElementById('sign-up-e-mail').value;
-        let password = document.getElementById('sign-up-password').value;
-        let address1 = document.getElementById('address1').value;
-        let address2 = document.getElementById('address1').value;
-        let phone1 = document.getElementById('phone1').value;
-        let phone2 = document.getElementById('phone2').value;
-        
-       
-        class User{
-            constructor(name, surname, email, password, address1, address2, phone1, phone2){
-                this.name = name,
-                this.surname = surname,
-                this.email = email,
-                this.password = password,
-                this.address1 = address1,
-                this.address2 = address2,
-                this.phone1 = phone1,
-                this.phone2 = phone2,
-                this.status = 1
-            }
-
-            static checkDoesUserExist(){
-                console.log(user)
-                alert('check user!')
-               
-                arrayUsers.forEach(element => {
-                    if (element.email === user.email) {
-                        alert('We alredy have that user in DB.')
-                        document.getElementById('sign-up-e-mail').value = '';
-                        user.email = '';
-                    }
-        
-                })
-
-            }
-        }
+    } else if(password !== passwordR) {
+        alert('Repeat password is not equal password')
+    } else { 
 
         const user = new User(name, surname, email, password, address1, address2, phone1, phone2);
-        User.checkDoesUserExist(user);
-        
-        //vadi niz iz local S i parsira u JavaScript, smesta u promenljivu nizKorisnika
-        /*var arrayUsers = JSON.parse(localStorage.getItem('bazakorisnika')) || [];
-        console.log(arrayUsers);*/
-
+        User.checkDoesUserExist(user, arrayUsers);        
        
-        if (user.email !== '') {
-            //ubacuje novi dogadjaj objekat u nizKorisnika        
+        if (user.email !== '') {        
             arrayUsers.push(user);
-            //smesta nizKorisnika u localStoride
             localStorage.setItem('localStorageUsers', JSON.stringify(arrayUsers));
-
-            console.log(arrayUsers);
-            console.log(user);
             clearSignUp();
-            document.getElementById('sign-up-form').style.display = 'none';
-            document.getElementById('go-sign-up-form').style.display = "block";
-            document.getElementById('go-sign-in-form').style.display = "none";
-            document.getElementById('sign-in-form').style.display = "block";
+            signUpForm.style.display = 'none';
+            goSignUpForm.style.display = "block";
+            goSignInForm.style.display = "none";
+            signInForm.style.display = "block";
         }
 
     }
 
-} //kraj funkcije registracija
+} //end SIGN UP function
